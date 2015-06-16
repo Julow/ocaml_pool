8d4f823d6aea74344980dd9e1890ae71f7029a2f
jaguillo <juloo.dsi@gmail.com>
2015-06-16 10:59:51 +0200

    ex07

---
+++ ex07/converges.ml
@@ -0,0 +1,30 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   converges.ml                                       :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2015/06/16 10:52:55 by jaguillo          #+#    #+#             *)
+(*   Updated: 2015/06/16 10:59:45 by jaguillo         ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+let rec converges f x n =
+	if n < 0 then
+		false
+	else if (f x) < x then (* lol *)
+		true
+	else
+		converges f x (n - 1)
+
+(*
+** Test
+*)
+let print_bool a =
+	print_string (if a then "true" else "false")
+
+let () =
+	print_bool (converges (( * ) 2) 2 5); print_char '\n';
+	print_bool (converges (fun x -> x / 2) 2 3); print_char '\n';
+	print_bool (converges (fun x -> x / 2) 2 2); print_char '\n';
