51c189cf554c8e32ba4fb179f2f3248b0904716c
jaguillo <juloo.dsi@gmail.com>
2015-06-17 11:27:20 +0200

    More test

--- ex01/crossover.ml
+++ ex01/crossover.ml
@@ -6,7 +6,7 @@
 (*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/17 11:09:42 by jaguillo          #+#    #+#             *)
-(*   Updated: 2015/06/17 11:21:29 by jaguillo         ###   ########.fr       *)
+(*   Updated: 2015/06/17 11:26:56 by jaguillo         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -25,16 +25,30 @@ let rec crossover l1 l2 =
 (*
 ** Test
 *)
-let rec printlist l =
-	match l with
-	| [] -> print_char '\n'
-	| head::sec::tail -> print_int head; print_string ", "; printlist (sec :: tail)
-	| head::tail -> print_int head; printlist tail
+let printlist l =
+	print_char '[';
+	let rec printl l =
+		match l with
+		| [] -> ()
+		| head::sec::tail -> print_int head; print_string "; "; printl (sec :: tail)
+		| head::tail -> print_int head; printl tail
+	in
+	printl l;
+	print_char ']'
+
+let test l1 l2 =
+	printlist l1;
+	print_string "  x  ";
+	printlist l2;
+	print_string "  =  ";
+	printlist (crossover l1 l2);
+	print_char '\n'
 
 let () =
-	printlist (crossover [1; 2; 3; 4; 5; 6] [2; 2; 8; 4]);
-	printlist (crossover [1; 2; 3; 4; 5; 6] [7; 8; 9; 10]);
-	printlist (crossover [2; 2; 3; 3; 4; 4] [2; 4]);
-	printlist (crossover [2; 2; 3; 3; 4; 4] [2; 4; 4]);
-	printlist (crossover [] [1; 2; 5; 3; 7; 4]);
-	printlist (crossover [1; 2; 5; 3; 7; 4] []);
+	test [1; 2; 3; 4; 5; 6] [2; 2; 8; 4];
+	test [1; 2; 3; 4; 5; 6] [7; 8; 9; 10];
+	test [2; 2; 3; 3; 4; 4] [2; 4];
+	test [2; 2; 3; 3; 4; 4] [2; 4; 4];
+	test [] [1; 2; 5; 3; 7; 4];
+	test [1; 2; 5; 3; 7; 4] [];
+	test [] [];
