8674e10022821cb56a318e30ab5250a132705df2
jaguillo <juloo.dsi@gmail.com>
2015-06-17 19:52:23 +0200

    nan

--- ex08/ft_sum.ml
+++ ex08/ft_sum.ml
@@ -6,7 +6,7 @@
 (*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/16 11:00:10 by jaguillo          #+#    #+#             *)
-(*   Updated: 2015/06/16 11:06:32 by jaguillo         ###   ########.fr       *)
+(*   Updated: 2015/06/17 19:51:30 by jaguillo         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -17,7 +17,10 @@ let rec ft_sum e min max =
 		else
 			lol (i + 1) (acc +. (e i))
 	in
-	lol min 0.
+	if min > max then
+		nan
+	else
+		lol min 0.
 
 (*
 ** Test
@@ -26,3 +29,6 @@ let () =
 	print_float (ft_sum (fun i -> float_of_int (i * i)) 1 10); print_char '\n';
 	print_float (ft_sum (fun i -> float_of_int i) 1 10); print_char '\n';
 	print_float (ft_sum (fun i -> 1.) 1 10); print_char '\n';
+	print_float (ft_sum (fun i -> 1.) 2 2); print_char '\n';
+	print_float (ft_sum (fun i -> 1.) 2 1); print_char '\n';
+	print_float (ft_sum (fun i -> 1.) 0 0); print_char '\n';
