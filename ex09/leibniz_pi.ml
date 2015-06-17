79c514df9ecd9c3542401a82bd878ddda60e3212
jaguillo <juloo.dsi@gmail.com>
2015-06-17 19:54:24 +0200

    Forbidden function

--- ex09/leibniz_pi.ml
+++ ex09/leibniz_pi.ml
@@ -6,23 +6,23 @@
 (*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/16 11:14:47 by jaguillo          #+#    #+#             *)
-(*   Updated: 2015/06/16 15:12:14 by jaguillo         ###   ########.fr       *)
+(*   Updated: 2015/06/17 19:54:08 by jaguillo         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
 let leibniz_pi delta =
 	let pi4 = 4. *. atan 1. in
-	let rec loop i pi =
+	let rec loop i j pi =
 		let d = pi *. 4. -. pi4 in
 		if d > (-.delta) && d < delta then
-			int_of_float i
+			j
 		else
-			loop (i +. 1.) (((-1.) ** i) /. (2. *. i +. 1.) +. pi)
+			loop (i +. 1.) (j + 1) (((-1.) ** i) /. (2. *. i +. 1.) +. pi)
 	in
 	if delta <= 0. then
 		-1
 	else
-		loop 0. 0.
+		loop 0. 0 0.
 (*
 ** Test
 *)
@@ -33,4 +33,5 @@ let () =
 	print_int (leibniz_pi 0.0001); print_char '\n';
 	print_int (leibniz_pi 0.00001); print_char '\n';
 	print_int (leibniz_pi 0.000001); print_char '\n';
-	print_int (leibniz_pi 0.0000001); print_char '\n';
+	print_int (leibniz_pi 0.0000002); print_char '\n';
+	print_int (leibniz_pi 0.00000005); print_char '\n';
