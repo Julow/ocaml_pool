8cc5a2fbbebe64d9b1872be96c5fd24649945f34
jaguillo <juloo.dsi@gmail.com>
2015-06-19 11:03:46 +0200

    ex02

--- ex02/uncipher.ml
+++ ex02/uncipher.ml
@@ -6,7 +6,17 @@
 (*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/18 19:23:33 by jaguillo          #+#    #+#             *)
-(*   Updated: 2015/06/18 19:23:33 by jaguillo         ###   ########.fr       *)
+(*   Updated: 2015/06/19 11:01:36 by jaguillo         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
+let rotc n c = char_of_int (((int_of_char c) + n + 128) mod 128)
+
+let unrot42 str = String.map (rotc (-42)) str
+
+let uncaesar n str = String.map (rotc (-n)) str
+
+let rec ft_uncrypt str fn =
+	match fn with
+	| []				-> str
+	| head::tail		-> head (ft_uncrypt str tail)
