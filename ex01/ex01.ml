d8b221128f8c851b641c0722f9db772b074526ed
jaguillo <juloo.dsi@gmail.com>
2015-06-24 19:23:47 +0200

    djb2

--- ex01/ex01.ml
+++ ex01/ex01.ml
@@ -6,15 +6,25 @@
 (*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/23 13:35:02 by jaguillo          #+#    #+#             *)
-(*   Updated: 2015/06/23 13:41:47 by jaguillo         ###   ########.fr       *)
+(*   Updated: 2015/06/24 19:23:34 by jaguillo         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
+
+let djb2 str =
+	let str_len = String.length str in
+	let rec loop i acc =
+		if i >= str_len then
+			acc
+		else
+			loop (i + 1) ((acc lsl 5) + acc + int_of_char ((String.get str i)))
+	in
+	loop 0 5381
 
 module StringHashtbl =
 	Hashtbl.Make (
 		struct
 			type t = string
-			let hash = Hashtbl.hash
+			let hash = djb2
 			let equal = (=)
 		end)
 
