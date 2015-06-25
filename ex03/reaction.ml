9da72ad371378c44bdeb7c879c2f6842644d5a79
jaguillo <juloo.dsi@gmail.com>
2015-06-25 18:53:03 +0200

    ex03

---
+++ ex03/reaction.ml
@@ -0,0 +1,22 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   reaction.ml                                        :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2015/06/25 16:44:58 by jaguillo          #+#    #+#             *)
+(*   Updated: 2015/06/25 17:15:31 by jaguillo         ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+class virtual reaction = object
+	method virtual get_start : (Molecule.molecule * int) list
+	method virtual get_result : (Molecule.molecule * int) list
+	method virtual balance : reaction
+	method virtual is_balanced : bool
+end
+
+(*
+** Nothing to test lol
+*)
