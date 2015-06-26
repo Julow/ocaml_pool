4c7da7859fbb1ef26f7b58a3ab4c36c57257ecd8
jaguillo <juloo.dsi@gmail.com>
2015-06-26 15:42:54 +0200

    Fix types

--- ex00/watchover.ml
+++ ex00/watchover.ml
@@ -6,7 +6,7 @@
 (*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/26 15:24:03 by jaguillo          #+#    #+#             *)
-(*   Updated: 2015/06/26 15:33:50 by jaguillo         ###   ########.fr       *)
+(*   Updated: 2015/06/26 15:42:45 by jaguillo         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -14,8 +14,8 @@ type hour = int
 
 let zero:hour = 0
 
-let add a b :hour = abs ((a + b) mod 12)
+let add (a:hour) (b:hour) :hour = abs ((a + b) mod 12)
 
-let sub a b :hour = abs ((a - b) mod 12)
+let sub (a:hour) (b:hour) :hour = abs ((a - b + 12) mod 12)
 
-let to_int (a:hour) = a
+let to_int (a:hour) :int = a
