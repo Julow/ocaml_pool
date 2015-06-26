65b6f4234777856e597b09ca6f7215522650dc49
jaguillo <juloo.dsi@gmail.com>
2015-06-26 16:51:45 +0200

    Fix watchover

--- ex00/watchover.ml
+++ ex00/watchover.ml
@@ -6,16 +6,21 @@
 (*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/06/26 15:24:03 by jaguillo          #+#    #+#             *)
-(*   Updated: 2015/06/26 15:42:45 by jaguillo         ###   ########.fr       *)
+(*   Updated: 2015/06/26 16:51:29 by jaguillo         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
 type hour = int
 
-let zero:hour = 0
+let zero:hour = 12
 
-let add (a:hour) (b:hour) :hour = abs ((a + b) mod 12)
+let add (a:hour) (b:hour) :hour =
+	let tmp = (a + b) mod zero in
+	if tmp <= 0 then
+		zero + tmp
+	else
+		tmp
 
-let sub (a:hour) (b:hour) :hour = abs ((a - b + 12) mod 12)
+let sub (a:hour) (b:hour) :hour = add a (-b)
 
 let to_int (a:hour) :int = a
